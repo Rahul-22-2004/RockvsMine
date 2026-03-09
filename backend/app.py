fd654454d452abc4df8b6e66128cf0cc13d6c652
Rahul-22-2004 <rahuldgowda2004@gmail.com>
2026-03-09 21:58:55 +0530

    Fix CORS for frontend

--- backend/app.py
+++ backend/app.py
@@ -65,9 +65,7 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:3000",
-    "https://rockvs-mine.vercel.app"], # frontend
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -15,7 +15,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    return Promise.reject(error); // 🔥 DO NOT STRIP RESPONSE
+    return Promise.reject(error); 
   }
 );
 
